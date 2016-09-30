dfd8e5e348f3665d456f7afa0a41a95b5fe215a0
lishengze <lishengz.cool@hotmail.com>
2016-09-30 15:22:15 +0800

    修改binding.byp

--- charset-convert-windows.h
+++ charset-convert-windows.h
@@ -9,67 +9,10 @@ using std::cout;
 using std::endl;
 using std::string;
 
-const int CHAR_BUFSIZE = 4096;       
-
-void Gb2312ToUtf8(const char *pcGb2312, std::string & strUtf8)
-
-{
-
-	//wchar_t pcUnicode[CHAR_BUFSIZE];
-
-	//gb2312 to unicode
-
-	int nUnicodeLen = MultiByteToWideChar(CP_ACP, 0, pcGb2312, -1, NULL, 0);
-
-	wchar_t * pcUnicode = new wchar_t[nUnicodeLen + 1];
-	memset(pcUnicode, 0, nUnicodeLen * 2 + 2);
-
-	MultiByteToWideChar(CP_ACP, 0, pcGb2312, -1, pcUnicode, nUnicodeLen);
-
-
-	//unicode to utf8
-
-	int nUtf8Len = WideCharToMultiByte(CP_UTF8, 0, pcUnicode, -1, NULL, 0, NULL, NULL);
-
-	char *pcUtf8 = new char[nUtf8Len + 1];
-	memset(pcUtf8, 0, nUtf8Len + 1);
-
-	WideCharToMultiByte(CP_UTF8, 0, pcUnicode, -1, pcUtf8, nUtf8Len, NULL, NULL);
-
-	strUtf8 = pcUtf8;
-	delete[] pcUtf8;
-	delete[] pcUnicode;
-}
+void Gb2312ToUtf8(const char *pcGb2312, std::string & strUtf8);
 
 /** UTF8TOGB2312*/
-void Utf8ToGb2312(const char *pcUtf8, std::string & strGB2312)
-
-{
-
-	//wchar_t pcUnicode[CHAR_BUFSIZE];
-
-	int nUnicodeLen = MultiByteToWideChar(CP_UTF8, 0, pcUtf8, -1, NULL, 0);
-
-	wchar_t * pcUnicode = new wchar_t[nUnicodeLen + 1];
-	memset(pcUnicode, 0, nUnicodeLen * 2 + 2);
-
-	//utf8 to unicode
-
-	MultiByteToWideChar(CP_UTF8, 0, pcUtf8, -1, pcUnicode, nUnicodeLen);
-
-	//unicode to gb2312
-
-	int nGb2312Len = WideCharToMultiByte(CP_ACP, 0, pcUnicode, -1, NULL, 0, NULL, NULL);
-
-	char *pcGb2312 = new char[nGb2312Len + 1];
-	memset(pcGb2312, 0, nGb2312Len + 1);
-
-	WideCharToMultiByte(CP_ACP, 0, pcUnicode, -1, pcGb2312, nGb2312Len, NULL, NULL);
-
-	strGB2312 = pcGb2312;
-	delete[] pcGb2312;
-	delete[] pcUnicode;
-}
+void Utf8ToGb2312(const char *pcUtf8, std::string & strGB2312);
 
 // std::string StringToUtf8(const std::string& strSource)
 // {
